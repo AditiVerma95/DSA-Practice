4f3dd48d961ad625247a5add50faf9d25fe1070a
Aditi Verma <anvil557@gmail.com>
2025-04-14 10:58:08 +0530

    merge sort

--- SortingAlgo/src/Main.java
+++ SortingAlgo/src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         int[] arr= {7, 10, 2, 11, 69};
         //bubbleSort(arr);
         //selectionSort(arr);
-        insertionSort(arr);
+        //insertionSort(arr);
+        mergeSort(arr);
         System.out.println(Arrays.toString(arr));
 
     }
@@ -48,6 +49,51 @@ public class Main {
                     arr[j] = temp;
                 }
             }
+        }
+    }
+    //mergeSort (7, 10, 2, 11, 69)
+    static void mergeSort(int[] arr){
+        if (arr.length < 2) {
+            return;
+        }
+        int n = arr.length;
+        int mid = n / 2;
+        int[] leftArr = new int[mid];
+        int[] rightArr = new int[n - mid];
+        for(int i = 0; i < mid; i++){
+            leftArr[i] = arr[i];
+        }
+        for(int i = mid; i < n; i++){
+            rightArr[i - mid] = arr[i];
+        }
+        mergeSort(leftArr);
+        mergeSort(rightArr);
+        merge(leftArr, arr, rightArr);
+    }
+    static void merge(int[] leftArr, int[] arr, int[] rightArr){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while(i < leftArr.length && j < rightArr.length){
+            if(leftArr[i] <= rightArr[j]){
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else{
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        while(i < leftArr.length){
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+        while(j < rightArr.length){
+            arr[k] = rightArr[j];
+            j++;
+            k++;
         }
     }
 }
